ace1bf4c25eae3db8afd1ae958452cbd6c5c884c
Docta Rhyf <drrhyf@gmail.com>
2023-11-09 20:40:58 +0200

    design fix

--- app/vehicules/page.tsx
+++ app/vehicules/page.tsx
@@ -6,10 +6,10 @@ function Vehicules({ searchParams }: { searchParams: any }) {
 
   return (
     <div>
-      <div className=" overflow-hidden relative p-4 min-h-[240pt] text-white bg-pink-400 ">
+      <div className=" overflow-hidden relative p-4 min-h-[240pt] text-white bg-black ">
         <div className=" flex   ">
           <Image
-            className=" bg-red-300 flex-grow absolute left-0 object-center  object-fill top-0 "
+            className=" opacity-50 h-full w-full  flex-grow absolute left-0 object-center  object-cover top-0 "
             src={"/alfa.webp"}
             width={1024}
             height={1024}
@@ -17,7 +17,13 @@ function Vehicules({ searchParams }: { searchParams: any }) {
           />
         </div>
 
-        <div className="text-white text-5xl absolute ">Vehicule</div>
+        <div className="text-white text-5xl  absolute left-0 top-0 w-full p-4 bg-gradient-to-b from-black to-black/0 ">
+          Vehicule
+        </div>
+
+        <p className=" bg-black/50 rounded-md p-4 text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+          Pour tous vos deals de vehicules
+        </p>
       </div>
     </div>
   );
